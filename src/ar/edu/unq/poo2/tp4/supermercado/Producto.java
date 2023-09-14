c4a96f164ca465bbc47be46327251c1d4f7511dc
Alejandra Besel <alejandrabesel24@gmail.com>
2023-09-14 20:41:32 -0300

    Add Producto

--- src/ar/edu/unq/poo2/tp4/supermercado/Producto.java
+++ src/ar/edu/unq/poo2/tp4/supermercado/Producto.java
@@ -1,5 +1,43 @@
 package ar.edu.unq.poo2.tp4.supermercado;
 
-public class Producto {
 
+public class Producto {
+	
+	String nombre;
+	Double precio;
+	boolean esPrecioCuidado = false;
+	
+	public Producto(String nombreProducto, Double precioProducto, boolean esCuidado ) {
+		this.setValues(nombreProducto, precioProducto);
+		esPrecioCuidado = esCuidado;
+	}
+	
+	public Producto(String nombreProducto, Double precioProducto) {
+		this.setValues(nombreProducto, precioProducto);
+	}
+	
+	private void setValues(String nombreProducto, Double precioProducto) {
+		nombre = nombreProducto;
+		precio = precioProducto;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public Double getPrecio() {
+		return precio;
+	}
+	
+	public boolean esPrecioCuidado() {
+		return esPrecioCuidado;
+	}
+	
+	public void esPrecioCuidado(boolean esCuidado) {
+		esPrecioCuidado = esCuidado;
+	}
+	
+	public void aumentarPrecio(Double aumento) {
+		precio += aumento;
+	}
 }
--- src/ar/edu/unq/poo2/tp4/supermercado/Supermercado.java
+++ src/ar/edu/unq/poo2/tp4/supermercado/Supermercado.java
@@ -1,5 +1,17 @@
 package ar.edu.unq.poo2.tp4.supermercado;
 
 public class Supermercado {
+	
+	String nombre;
+	String direccion;
+	Producto arrayProductos[];
+	
+	public Supermercado(String nombreSupermercado, String direccionSupermercado) {
+		nombre = nombreSupermercado;
+		direccion = direccionSupermercado;
+	}
 
+	public int getCantidadDeProductos() {
+		return arrayProductos.lenght;
+	}
 }
